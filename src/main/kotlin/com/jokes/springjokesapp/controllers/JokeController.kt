f9ceb03c6d41b9af332aff4d6035cf3d257e5812
grjus <grjus@pm.me>
2021-08-12 10:40:46 +0200

    Added sample json output

--- src/main/kotlin/com/jokes/springjokesapp/controllers/JokeController.kt
+++ src/main/kotlin/com/jokes/springjokesapp/controllers/JokeController.kt
@@ -1,20 +1,28 @@
 package com.jokes.springjokesapp.controllers
-
+import com.jokes.springjokesapp.`data-model`.SampleJson
 import com.jokes.springjokesapp.services.JokeService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @Controller
 class JokeController(@Qualifier("chuckJoke") val jokeService: JokeService) {
-
-
 
     @RequestMapping("/","")
     fun showJoke(model:Model):String{
         model.addAttribute("joke",jokeService.getJoke())
         return "index"
+    }
+
+
+}
+@RestController
+class JsonController{
+    @GetMapping("/sample-json")
+    fun showJson():SampleJson{
+        return SampleJson("Grzegorz","Juszkiewicz",37)
     }
 }
---
+++ src/main/kotlin/com/jokes/springjokesapp/data-model/SampleJson.kt
@@ -0,0 +1,3 @@
+package com.jokes.springjokesapp.`data-model`
+
+data class SampleJson(val firstName:String, val secondName:String, val age:Int)
