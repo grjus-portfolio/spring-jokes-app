37e87e2c8e1d7b99831bb5c30e783f4b7abd3014
grjus <grjus@pm.me>
2021-08-12 10:34:31 +0200

    Added Joke Controller interface

--- src/main/kotlin/com/jokes/springjokesapp/controllers/JokeController.kt
+++ src/main/kotlin/com/jokes/springjokesapp/controllers/JokeController.kt
@@ -1,10 +1,20 @@
 package com.jokes.springjokesapp.controllers
 
+import com.jokes.springjokesapp.services.JokeService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class JokeController {
-    fun joke(){
-        return JokeService.getJoke()
+class JokeController(@Qualifier("chuckJoke") val jokeService: JokeService) {
+
+
+
+    @RequestMapping("/","")
+    fun showJoke(model:Model):String{
+        model.addAttribute("joke",jokeService.getJoke())
+        return "index"
     }
 }
